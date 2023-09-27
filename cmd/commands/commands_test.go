fde29aa09b972984288f17c9f1b0c65422360c50
Thorsten de Buhr <thorsten.deBuhr@arm.com>
2023-09-27 10:35:05 +0200

    linter

--- cmd/commands/commands_test.go
+++ cmd/commands/commands_test.go
@@ -6,48 +6,5 @@
 
 package commands_test
 
-import (
-	"fmt"
-	"os"
-	"path/filepath"
-	"strings"
-	"testing"
-
-	log "github.com/sirupsen/logrus"
-	"github.com/stretchr/testify/assert"
-)
-
-// Copy of cmd/log.go
-type LogFormatter struct{}
-
-func (s *LogFormatter) Format(entry *log.Entry) ([]byte, error) {
-	level := strings.ToUpper(entry.Level.String())
-	msg := fmt.Sprintf("%s: %s\n", level[0:1], entry.Message)
-	return []byte(msg), nil
-}
-
-var testingDir = filepath.Join("..", "..", "testdata", "integration")
-
-type TestCase struct {
-	args           []string
-	name           string
-	defaultMode    bool
-	createPackRoot bool
-	expectedStdout []string
-	expectedStderr []string
-	expectedErr    error
-	setUpFunc      func(t *TestCase)
-	tearDownFunc   func()
-	validationFunc func(t *testing.T)
-	assert         *assert.Assertions
-	env            map[string]string
-}
-
 func init() {
-	logLevel := log.InfoLevel
-	if os.Getenv("LOG_LEVEL") == "debug" {
-		logLevel = log.DebugLevel
-	}
-	log.SetLevel(logLevel)
-	log.SetFormatter(new(LogFormatter))
 }
