09993634cdc059ce1b09ec68260a71624ed1fe97
Tyc Lukas NXP <lukas.tyc@nxp.com>
2024-05-14 15:56:25 +0200

    Simplyfis launch sript for MCUXpresso config tools (windows-adm64)
    Remove for legacy project with .mex file ins same folder as .cproject and .csolution

--- scripts/MCUXpresso_Config_Tools/windows-amd64/launch-MCUXpressoConfigTools.bat
+++ scripts/MCUXpresso_Config_Tools/windows-amd64/launch-MCUXpressoConfigTools.bat
@@ -1,11 +1,8 @@
 @echo off
 REM Script launch MCUXpresso Config Tools in OpenCMSIS generator mode. It is supported since MCUXpresso Config Tools v16
 
-REM get folder, where the script run
-set prjDir=%cd%
-
-REM Get folder from file path https://stackoverflow.com/questions/659647/how-to-get-folder-path-from-file-path-with-cmd
-set folder=%~dp1
+REM path to .cbuild-gen.idx.yml file
+set idxFile=%1
 
 REM  Get Config Tools Location
 set cmd=REG QUERY "HKEY_CLASSES_ROOT\NXP Semiconductors.MCUXpresso Config Tools.mex\shell\open\command" /ve
@@ -26,18 +23,7 @@ FOR /F "delims=" %%i IN ("%tools_path%") DO (
 )
 ENDLOCAL & SET "tools_folder=%tools_folder%"
 
-REM Check existenci of mex in run script folder
-for %%f in (%prjDir%/*.mex) do (
-    set "mexFile=%%f"
-)
-
-REM Launch tools from its folder
+REM Launch config tools from its folder
 pushd %tools_folder%
-if not defined mexFile (
-    REM run wihout mex file
-    %tools_path% -CreateFromProject %folder% -OpencmsisGeneratorCgen
-) else (
-    REM run with existing mex file
-    %tools_path% -Load %mexFile% -OpencmsisGeneratorCgen -ProjectLink %folder%
-)
+%tools_path% -CreateFromProject %idxFile% -OpencmsisGeneratorCgen
 popd
